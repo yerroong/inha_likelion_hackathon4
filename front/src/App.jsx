5d044f088598c9b2627f3d11943ae40ac11bc7e4
yerin <wbflqldks90@naver.com>
2024-06-29 01:27:32 +0900

    메인페이지 틀만

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomePage from './pages/Homepage';
+import MainPage from './pages/Mainpage';
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<MainPage />} />
       </Routes>
     </Router>
   );
