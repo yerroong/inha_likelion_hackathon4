14d49eacbeb7613d2defe222fb436b3337c29832
imh0ng <ghdgus0417@inha.edu>
2024-06-29 00:53:36 +0900

    header + 기타

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div>
-      <h1>Hello, React!</h1>
-    </div>
+    <Router>
+      <Header></Header>
+      <main>
+        <Routes>
+          <Route path="/" />
+          <Route path="/search" />
+          <Route path="/posting" />
+        </Routes>
+      </main>
+    </Router>
   );
 }
 
