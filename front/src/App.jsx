6afc0527ef57747c67f134611285f2db77b6242c
imh0ng <ghdgus0417@inha.edu>
2024-06-29 21:02:47 +0900

    search

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import MainPage from './pages/Mainpage';
-import Search from './pages/Search';
+import Search from './pages/search/Search';
 
 const App = () => {
   return (
