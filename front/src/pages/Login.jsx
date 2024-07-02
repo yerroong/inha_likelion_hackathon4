6b81b7274dc6d886fd18bc09129672c5bb133feb
imh0ng <ghdgus0417@inha.edu>
2024-07-02 16:42:18 +0900

    login

--- front/src/pages/Login.jsx
+++ front/src/pages/Login.jsx
@@ -20,33 +20,52 @@ const Subtitle = styled.p`
 `;
 
 const Button = styled.button`
-  width: 300px;
-  padding: 10px;
-  margin-bottom: 10px;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-  background-color: white;
-  cursor: pointer;
-  font-size: 16px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  gap: 10px;
+    width: 300px;
+    height: 55px;
+    padding: 10px;
+    margin-bottom: 10px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    background-color: white;
+    cursor: pointer;
+    font-size: 17px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: relative;
 
   &:hover {
     background-color: #f0f0f0;
+  }
+
+  img {
+    width: 20px;
+    height: 20px;
+    position: absolute;
+    left: 30px;
+  }
+
+  &.kakao-login-btn {
+    background-color: #fedc3f;
+  }
+
+  &.naver-login-btn {
+    background-color: #20c801;
+    color: #fff;
   }
 `;
 
 const EmailLoginButton = styled(Link)`
   width: 300px;
+  height: 55px;
   padding: 10px;
-  margin-top: 20px;
+  margin-bottom: 10px;
   border: 1px solid #ccc;
   border-radius: 5px;
+  box-sizing: border-box;
   background-color: white;
   cursor: pointer;
-  font-size: 16px;
+  font-size: 17px;
   text-decoration: none;
   color: black;
   display: flex;
@@ -64,21 +83,21 @@ const Login = () => {
     <Container>
       <Title>로그인하기</Title>
       <Subtitle>소셜 아이디 및 이메일로 로그인할 수 있어요.</Subtitle>
-      <Button>
-        <img src="/google-logo.png" alt="Google" />
-        Google로 시작하기
-      </Button>
-      <Button>
-        <img src="/kakao-logo.png" alt="Kakao" />
-        카카오로 시작하기
-      </Button>
-      <Button>
-        <img src="/naver-logo.png" alt="Naver" />
-        네이버로 시작하기
-      </Button>
       <EmailLoginButton to="/email-login">
         이메일로 로그인하기
       </EmailLoginButton>
+      <Button className='google-login-btn'>
+        <img src="/google-logo.png" alt="Google" />
+        Google로 시작하기
+      </Button>
+      <Button className='kakao-login-btn'>
+        <img src="/kakao-logo.png" alt="Kakao" />
+        카카오로 시작하기
+      </Button>
+      <Button className='naver-login-btn'>
+        <img src="/naver-logo.png" alt="Naver" />
+        네이버로 시작하기
+      </Button>
     </Container>
   );
 };
